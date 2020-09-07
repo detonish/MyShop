8b7619138c2a1bd218357e339048b69f50b58a6a
detonish <aragonpalma.nico@gmail.com>
2020-09-07 01:09:35 -0300

    almost done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container mt-5">
+    <h1>My Shop</h1> <FontAwesomeIcon></FontAwesomeIcon>
+    <Navbar
+      :cart="cart"
+      :cart-qty="cartQty"
+      :cart-total="cartTotal"
+      @toggle="toggleSliderStatus"
+      @delete="deleteItem"
+    ></Navbar>
+    <PriceSlider
+      :slider-status="sliderStatus"
+      :maximum.sync="maximum"
+    ></PriceSlider>
+
+    <product-list
+      :maximum="maximum"
+      :products="products"
+      @add="addItem"
+    ></product-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import ProductList from "@/components/ProductList.vue";
+import PriceSlider from "@/components/PriceSlider.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
-  name: "App",
+  name: "app",
   components: {
-    HelloWorld
+    FontAwesomeIcon,
+    ProductList,
+    PriceSlider,
+    Navbar
+  },
+  data: function() {
+    return {
+      maximum: 99,
+      sliderStatus: true,
+      cart: [],
+      products: null
+    };
+  },
+
+  computed: {
+    cartTotal: function() {
+      let sum = 0;
+      for (let key in this.cart) {
+        sum = sum + this.cart[key].product.price * this.cart[key].qty;
+      }
+      return sum;
+    },
+    cartQty: function() {
+      let qty = 0;
+      for (let key in this.cart) {
+        qty = qty + this.cart[key].qty;
+      }
+      return qty;
+    }
+  },
+  methods: {
+    toggleSliderStatus: function() {
+      this.sliderStatus = !this.sliderStatus;
+    },
+    deleteItem: function(id) {
+      if (this.cart[id].qty > 1) {
+        this.cart[id].qty--;
+      } else {
+        this.cart.splice(id, 1);
+      }
+    },
+    addItem: function(product) {
+      var whichProduct;
+      var existing = this.cart.filter(function(item, index) {
+        if (item.product.id == Number(product.id)) {
+          whichProduct = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (existing.length) {
+        this.cart[whichProduct].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    }
+  },
+
+  mounted: function() {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then(response => response.json())
+      .then(data => {
+        this.products = data;
+      });
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1s ease-in-out;
 }
 </style>
